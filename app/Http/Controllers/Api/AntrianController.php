3a76cefc72e462b22aa2379c03625b28c418c736
bramantyaadi <bramantyaadi44@gmail.com>
2021-06-05 10:32:11 +0700

    add antrian fix

--- app/Http/Controllers/Api/AntrianController.php
+++ app/Http/Controllers/Api/AntrianController.php
@@ -24,6 +24,45 @@ class AntrianController extends Controller
                         'status' => 500
                     ]);
         }
-        
+    }
+    public function store(Request $request)
+    {
+        try {
+            $data_max = antrian::max('index')+1;
+            if($data_max >= 10){
+                $nomor_antrian = "A0".$data_max;
+            }
+            else if ($data_max < 10 ) {
+                $nomor_antrian = "A00".$data_max;
+            }
+
+
+            try {
+                $antrian = new antrian;
+                $antrian->index = $data_max;
+                $antrian->nomor_antrian = $nomor_antrian;
+                $antrian->save();
+
+                return response()->json([
+                    "status" => 200,
+                    "response" => true,
+                    "data_max" => $data_max,
+                    "nomor_antrian" => $nomor_antrian,
+                    "time" => date("H:i:s")
+                ]);
+            } catch (\Throwable $th) {
+                return response()->json([
+                    "response" => $th,
+                    "status" => 500
+                ]);
+            }
+            
+        } catch (\Throwable $th) {
+            //throw $th;
+            return response()->json([
+                "response" => $th,
+                'status' => 500
+            ]);
+        }
     }
 }
